818934ba5d550de91925041452e367491166110e
javiers78889 <javiers78889@gmail.com>
2025-01-06 14:22:41 -0500

    ok

--- backend/src/routes/budgetRouter.ts
+++ backend/src/routes/budgetRouter.ts
@@ -3,7 +3,8 @@ import { BudgetController } from '../controllers/BudgetController'
 import { handleInputErrors } from '../middleware/validation'
 import { findbyId } from '../middleware/findById'
 import { bodyValidations, paramValidations } from '../middleware/ExpressValidator'
-import { body } from 'express-validator'
+import Expense from '../models/Expense'
+import { ExpensesController } from '../controllers/ExpenseController'
 
 
 const router = Router()
@@ -14,14 +15,19 @@ router.param('budgetId', findbyId)
 
 
 router.get('/', BudgetController.getAll)
-router.post('/',bodyValidations, handleInputErrors, BudgetController.create)
+router.post('/', bodyValidations, handleInputErrors, BudgetController.create)
 router.get('/:budgetId', BudgetController.getById)
-router.put('/:budgetId',bodyValidations,  handleInputErrors, BudgetController.editById)
+router.put('/:budgetId', bodyValidations, handleInputErrors, BudgetController.editById)
 router.delete('/:budgetId', BudgetController.deleteById)
 
 
+/**Gastos **/
 
-
+router.post('/:budgetId/expenses', ExpensesController.getAll)
+router.post('/:budgetId/expenses/:id', ExpensesController.create)
+router.get('/:budgetId/expenses/:id', ExpensesController.getById)
+router.put('/:budgetId/expenses/:id', ExpensesController.updateById)
+router.delete('/:budgetId/expenses/:id', ExpensesController.deleteById)
 
 
 
