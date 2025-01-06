5664346bdf1cc8f0674f764cd5fe839bd1067aa1
javiers78889 <javiers78889@gmail.com>
2025-01-05 23:16:53 -0500

    primer commit

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -1,5 +1,7 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { BudgetController } from '../controllers/BudgetController'
+import { body } from 'express-validator'
+import { handleInputErrors } from '../middleware/validation'
 
 
 const router = Router()
@@ -7,11 +9,12 @@ const router = Router()
 
 
 
-router.get('/',BudgetController.getAll)
-router.post('/',BudgetController.create)
-router.get('/:id',BudgetController.getById)
-router.put('/:id',BudgetController.editById)
-router.delete('/:id',BudgetController.deleteById)
+router.get('/', BudgetController.getAll)
+router.post('/', body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+body('amount').isNumeric().notEmpty().withMessage('La cantidad no puede ir vacia'), handleInputErrors, BudgetController.create)
+router.get('/:id', BudgetController.getById)
+router.put('/:id', BudgetController.editById)
+router.delete('/:id', BudgetController.deleteById)
 
 
 
