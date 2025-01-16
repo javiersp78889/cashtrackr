bb8f34a64f3aeba79984e763c38eb85b7dc6e68c
javiers78889 <javiers78889@gmail.com>
2025-01-16 03:49:04 +0100

    n

--- src/utils/checkpassword.ts
+++ src/utils/checkpassword.ts
@@ -1,6 +1,16 @@
 import type { Request, Response, NextFunction } from "express"
+import { passwordVerify } from "./bcrypt"
+import Users from "../models/User"
 
 export const checkPassword = async (req: Request, res: Response, next: NextFunction) => {
-console.log(req.usuarios.id)
-        
+
+    const usuario = await Users.findByPk(req.usuarios.id)
+    const user = await passwordVerify(req.body.password, usuario.password)
+    if(user){
+        res.status(201).json("Autorizado")
+    }else{
+        res.status(501).json("Ups, parece que ese no era tu password")
+    }
+    
+
 }
